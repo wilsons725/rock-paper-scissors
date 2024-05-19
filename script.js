3587d6ea2d84b4809d8e210df5841d887498b762
wilsons725 <167576258+wilsons725@users.noreply.github.com>
2024-05-19 16:52:34 -0500

    Start project over
    Created function to return value of 'paper', 'rock', or scissors.
    Created a function to prompt the user to input 'paper', 'rock', or 'scissors'
    Changed the input so that the user input would always enter as lowercase.
    Created a function that would print to the console if the user or computer wins the round.

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-console.log("Hello World")
+"use strict";
 
-function getComputerChoice(number) {
-    // Array uses brackets in JS
-    let options = ["Rock", "Paper", "Scissors"]
-    return options [Math.floor(Math.random() * number)];
-}
+let humanScore = 0;
+let computerScore = 0;
 
+// When the user inputs a word, console will output a valid choice of rock, paper or scissors
+const getHumanChoice = function () {
+  let userInput = "";
+  // Do...While statement creates a loop of the prompt if the value entered doesn't equal rock paper or scissors.
+  do {
+    // uses .toLowerCase to limit 'or' options below by converting user input to lowercase.
+    userInput = prompt("Rock, Paper, Or Scissors?").toLowerCase();
+  } while (
+    userInput !== "rock" &&
+    userInput !== "paper" &&
+    userInput !== "scissors"
+  );
+  return userInput;
+};
+// console.log(getHumanChoice());
+// getHumanChoice();
 
-console.log (getComputerChoice(3));
+// When the user selects their option, the script will generate a random choice by leverage the math random function and selecting one of the options in an array
+const getComputerChoice = function () {
+  const options = ["Rock", "Paper", "Scissors"];
+  return options[Math.trunc(Math.random() * 3)].toLowerCase();
+};
+// console.log(getComputerChoice());
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+const playRound = function (humanChoice, computerChoice) {
+  if (humanSelection === "rock" && computerSelection === "paper") {
+    console.log("You Lose, paper beats rock!");
+  } else if (humanSelection === "rock" && computerSelection === "scissors") {
+    console.log("You Win, rock beats scissors!");
+  } else if (humanSelection === "rock" && computerSelection === "rock") {
+    console.log("Play Again, both selected rock!");
+  } else if (humanSelection === "paper" && computerSelection === "scissors") {
+    console.log("You Lose, scissors beats paper!");
+  } else if (humanSelection === "paper" && computerSelection === "rock") {
+    console.log("You Win, paper beats rock!");
+  } else if (humanSelection === "paper" && computerSelection === "paper") {
+    console.log("Play Again, both selected paper!");
+  } else if (humanSelection === "scissors" && computerSelection === "rock") {
+    console.log("You Lose, rock beats scissors!");
+  } else if (humanSelection === "scissors" && computerSelection === "paper") {
+    console.log("You Win, scissors beats paper!");
+  } else if (
+    humanSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    console.log("Play Again, both selected scissors!");
+  }
+};
+
+playRound(humanSelection, computerSelection);
