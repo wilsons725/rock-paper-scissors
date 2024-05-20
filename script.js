a84df1e0902eef3c72069d745a17f157b884ec70
wilsons725 <167576258+wilsons725@users.noreply.github.com>
2024-05-20 12:30:25 -0500

    Create a function that allows the game to be run 5 times.
    created the playGame function to loop the playRound function and alert who wins the game.
    moved previous functions under the playRound function so they would recall every iteration.

--- script.js
+++ script.js
@@ -1,58 +1,69 @@
 "use strict";
 
+alert(
+  "Welcome to Rock, Paper, Scissors! Open the console to view the results of each round. You will play 5 games, good luck!"
+);
+
 let humanScore = 0;
 let computerScore = 0;
 
-// When the user inputs a word, console will output a valid choice of rock, paper or scissors
-const getHumanChoice = function () {
-  let userInput = "";
-  // Do...While statement creates a loop of the prompt if the value entered doesn't equal rock paper or scissors.
-  do {
-    // uses .toLowerCase to limit 'or' options below by converting user input to lowercase.
-    userInput = prompt("Rock, Paper, Or Scissors?").toLowerCase();
-  } while (
-    userInput !== "rock" &&
-    userInput !== "paper" &&
-    userInput !== "scissors"
-  );
-  return userInput;
-};
-// console.log(getHumanChoice());
-// getHumanChoice();
+const playRound = function () {
+  const getHumanChoice = function () {
+    let userInput = "";
+    do {
+      userInput = prompt("Rock, Paper, Or Scissors?").toLowerCase();
+    } while (
+      userInput !== "rock" &&
+      userInput !== "paper" &&
+      userInput !== "scissors"
+    );
+    return userInput;
+  };
 
-// When the user selects their option, the script will generate a random choice by leverage the math random function and selecting one of the options in an array
-const getComputerChoice = function () {
-  const options = ["Rock", "Paper", "Scissors"];
-  return options[Math.trunc(Math.random() * 3)].toLowerCase();
-};
-// console.log(getComputerChoice());
+  const getComputerChoice = function () {
+    const options = ["Rock", "Paper", "Scissors"];
+    return options[Math.trunc(Math.random() * 3)].toLowerCase();
+  };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
 
-const playRound = function (humanChoice, computerChoice) {
   if (humanSelection === "rock" && computerSelection === "paper") {
     console.log("You Lose, paper beats rock!");
+    computerScore++;
   } else if (humanSelection === "rock" && computerSelection === "scissors") {
     console.log("You Win, rock beats scissors!");
-  } else if (humanSelection === "rock" && computerSelection === "rock") {
-    console.log("Play Again, both selected rock!");
+    humanScore++;
   } else if (humanSelection === "paper" && computerSelection === "scissors") {
     console.log("You Lose, scissors beats paper!");
+    computerScore++;
   } else if (humanSelection === "paper" && computerSelection === "rock") {
     console.log("You Win, paper beats rock!");
-  } else if (humanSelection === "paper" && computerSelection === "paper") {
-    console.log("Play Again, both selected paper!");
+    humanScore++;
   } else if (humanSelection === "scissors" && computerSelection === "rock") {
     console.log("You Lose, rock beats scissors!");
+    computerScore++;
   } else if (humanSelection === "scissors" && computerSelection === "paper") {
     console.log("You Win, scissors beats paper!");
-  } else if (
-    humanSelection === "scissors" &&
-    computerSelection === "scissors"
-  ) {
-    console.log("Play Again, both selected scissors!");
+    humanScore++;
+  } else {
+    console.log("Tie, Play Again!");
   }
 };
 
-playRound(humanSelection, computerSelection);
+const playGame = function () {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+  if (humanScore > computerScore) {
+    console.log(`You Win! ${humanScore} to ${computerScore}`);
+    alert(`You Win! ${humanScore} to ${computerScore}`);
+  } else if (humanScore < computerScore) {
+    console.log(`You Lose! ${computerScore} to ${humanScore}`);
+    alert(`You Lose! ${computerScore} to ${humanScore}`);
+  } else {
+    console.log(`Tie Game! ${humanScore} to ${computerScore}`);
+    alert(`Tie Game! ${humanScore} to ${computerScore}`);
+  }
+};
+playGame();
